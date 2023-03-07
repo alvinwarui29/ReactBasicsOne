14ef1536c46373e2ce0d429a6d33900828abbda6
warui maina <87015030+alvinwarui29@users.noreply.github.com>
2023-03-08 02:17:10 +0300

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const day = new Date();
+const year = day.getFullYear();
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright {year}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Title</h1>
+      <p>This is the content of the first keeper app</p>
+    </div>
+  );
+}
+export default Note;
